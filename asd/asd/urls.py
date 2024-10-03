b0f4b2e501301abe75ec1df5cf2809af23d236b8
Vincent Veri <vincent.veri@gmail.com>
2024-10-03 20:28:43 +0200

    crea index predefinita

--- asd/asd/urls.py
+++ asd/asd/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('persone/', include('persone.urls', namespace='persone'))
+    path('persone/', include('persone.urls', namespace='persone')),
+    path('', index, name='index')
 ]
