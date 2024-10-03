1d0036660396b5de8aa64e0688acdb96a32dc7da
Vincent Veri <vincent.veri@gmail.com>
2024-10-03 19:24:59 +0200

    aggiungi paginazione risultati

--- asd/persone/views.py
+++ asd/persone/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Persona
 
+
 def persona_list(request):
-    persone = Persona.attivo.all()
+    object_list = Persona.attivo.all()
+    paginator = Paginator(object_list, 1)
+    page = request.GET.get('page')
+    try:
+        persone = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        persone = paginator.page(1)
+    except EmptyPage:
+        persone = paginator.page(paginator.num_pages)
     return render(request,
                   'persone/persona/list.html',
-                  {'persone': persone})
+                  {'page': page,
+                      'persone': persone})
 
 def persona_detail(request, id):
     persona = get_object_or_404(Persona, id=id)
